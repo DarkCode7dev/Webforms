a6dabe7f715f6855295ec3f9d13701d05fa47fa9
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2021-03-18 23:32:22 +0530

    Create Ado_Insert.cs
    Day 26

---
+++ Ado_Insert.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Webforms
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=Batch;Integrated Security=True");
+            con.Open();
+           
+            SqlCommand cmd = new SqlCommand("insert into customer values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + TextBox4.Text + "','" + TextBox5.Text + "'," + TextBox6.Text + ")",con);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
